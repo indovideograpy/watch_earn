d4f06a865a9ee1f584658c52cb7eb45de79410fb
D0i-Rd <40250922+D0i-Rd@users.noreply.github.com>
2018-11-12 22:05:20 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
-
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
 $ticket = "xxxxxxxxxxxxxxxxxx";
 $meid = "xxxxxxxxxxxxxxxxxx";
 $os="xxxxxxxxxxxxxxxxxx";
